3a96d68cb087e37245c9fb065ed7c89569f2cc6f
mjjq <marcus_quantrill@hotmail.com>
2019-10-17 15:56:44 +0100

    Fixed algorithm to work properly

--- collision.frag
+++ collision.frag
@@ -1,5 +1,29 @@
+
+uniform int nSpheres;
+uniform vec2[1024] positions;
+uniform float radius;
+
+
+bool isIntersecting(int sphereX, int sphereY)
+{
+    float squareDist = dot(positions[sphereX] - positions[sphereY],
+                           positions[sphereX] - positions[sphereY]);
+
+    if(squareDist <= 4.0*radius*radius)
+        return true;
+
+    return false;
+}
+
 void main()
 {
-    vec2 coord = gl_TexCoord[0].xy;
-    gl_FragColor = vec4(coord, 1.0, 1.0);
+    int sphere1 = int(gl_TexCoord[0].x * float(nSpheres));
+    int sphere2 = int((1.0-gl_TexCoord[0].y) * float(nSpheres));
+
+    if(isIntersecting(sphere1, sphere2))
+        gl_FragColor = vec4(1.0);
+    else
+        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+
+    //gl_FragColor = vec4(gl_TexCoord[0].xy, 0.0, 1.0);
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <vector>
 #include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
+#include <stdlib.h>
 
 struct Sphere
 {
@@ -18,28 +20,40 @@ class CollisionDetector
     sf::Shader collisionShader;
     sf::Sprite emptySprite;
 
+    sf::RectangleShape debugShape;
+
     std::vector<sf::Glsl::Vec2 > spherePositions;
     std::vector<sf::Vector2f > sphereVelocities;
     std::vector<sf::Color > sphereColors;
     std::vector<std::pair<int, int> > collidingPairs;
 
+    int numSpheres = 0;
+
     float radius = 5.0f;
 
 public:
-    CollisionDetector(int noSpheres, sf::Vector2f _worldSize) : worldSize{_worldSize}
+    CollisionDetector(int noSpheres, sf::Vector2f _worldSize) : worldSize{_worldSize}, numSpheres{noSpheres}
     {
         for(int i=0; i<noSpheres; ++i)
         {
-            spherePositions.push_back({0.0f, 0.0f});
-            sphereVelocities.push_back({0.0f, 0.0f});
+            float scale = 10.0f;
+            float random = (float)rand()/(float)RAND_MAX;
+            spherePositions.push_back(worldSize/2.0f);
+            sphereVelocities.push_back(scale*sf::Vector2f{random, random*random});
             sphereColors.push_back(sf::Color::Red);
         }
         collisionPairTexture.create(noSpheres, noSpheres);
+        emptySprite.setTexture(collisionPairTexture.getTexture());
+        emptySprite.setScale({10.0f, 10.0f});
+
+        debugShape.setSize({100.0f, 100.0f});
+        debugShape.setPosition({100.0f, 100.0f});
 
         if (!collisionShader.loadFromFile("collision.frag", sf::Shader::Fragment))
         {
             std::cout << "error loading shader\n";
         }
+                //collisionShader.setUniform("numberOfSpheres", 5);
     }
 
     void integratePositions(float dt)
@@ -60,25 +74,29 @@ public:
 
     void getCollidingPairsFromTexture(const sf::Texture & collidingTexture)
     {
+        collidingPairs.clear();
         auto image = collidingTexture.copyToImage();
 
         for(int i=0; i<collidingTexture.getSize().x; ++i)
-            for(int j=i+1; j<collidingTexture.getSize().x; ++j)
+            for(int j=i+1; j<collidingTexture.getSize().y; ++j)
             {
-                if(image.getPixel(i,j) == sf::Color::White)
+                if(image.getPixel(j,i) == sf::Color::White)
+                {
+                    //std::cout << "i,j: " << i << ", " << j << "\n";
                     collidingPairs.push_back({i,j});
+                }
             }
+
+        //std::cout << collidingPairs.size() << " collpairs\n";
     }
 
     void detectCollisions()
     {
-        for(int i=0; i<sphereColors.size(); ++i)
-        {
-            sphereColors[i] = sf::Color::Red;
-        }
 
-        //collisionShader.setUniformArray("positions", spherePositions.data(), spherePositions.size());
-        //collisionShader.setUniform("result", radius);
+        collisionShader.setUniformArray("positions", spherePositions.data(), spherePositions.size());
+        collisionShader.setUniform("nSpheres", (int)spherePositions.size());
+        collisionShader.setUniform("radius", radius);
+
 
         collisionPairTexture.draw(emptySprite, &collisionShader);
 
@@ -87,13 +105,22 @@ public:
 
     void resolveCollisions()
     {
+        for(int i=0; i<sphereColors.size(); ++i)
+        {
+            sphereColors[i] = sf::Color::Red;
+        }
+
         for(int i=0; i<collidingPairs.size(); ++i)
         {
             int collider1 = collidingPairs[i].first;
             int collider2 = collidingPairs[i].second;
 
-            sphereColors[collider1] = sf::Color::Green;
-            sphereColors[collider2] = sf::Color::Green;
+            if(collider1 != collider2)
+            {
+                sphereColors[collider1] = sf::Color::Green;
+                sphereColors[collider2] = sf::Color::Green;
+
+            }
         }
     }
 
@@ -110,7 +137,11 @@ public:
             window.draw(circle);
         }
 
-        window.draw(emptySprite, &collisionShader);
+        emptySprite.setScale({200.0f/numSpheres, 200.0f/numSpheres});
+        emptySprite.setPosition({200.0f, 200.0f});
+        window.draw(emptySprite);
+        emptySprite.setPosition({00.0f, 00.0f});
+        emptySprite.setScale({1.0f, 1.0f});
     }
 
 };
@@ -121,20 +152,28 @@ int main()
     sf::VideoMode mode(800, 800);
     window.create(mode, "test");
 
-    //CollisionDetector test(1000, {100.0f, 100.0f});
+    CollisionDetector test(150, {100.0f, 100.0f});
 
     sf::Shader shader;
     shader.loadFromFile("collision.frag", sf::Shader::Fragment);
     sf::RenderTexture texture;
-    texture.create(1000, 1000);
+    texture.create(20,20);
 
     sf::Sprite emptySprite;
+    emptySprite.setTexture(texture.getTexture());
     texture.draw(emptySprite, &shader);
+    emptySprite.setScale({20.f, 20.0f});
 
     sf::RectangleShape shape;
     shape.setPosition(100.0f, 100.0f);
-    shape.setSize({100.0f, 100.0f});
+    shape.setSize({10.0f, 10.0f});
+    shape.setScale({10.0f, 10.0f});
+
     shape.setTexture(&texture.getTexture());
+
+    srand(420000);
+
+    bool isPaused = false;
 
     while(window.isOpen())
     {
@@ -147,20 +186,33 @@ int main()
                 case(sf::Event::Closed):
                     window.close();
                     break;
+                case(sf::Event::KeyPressed):
+
+                    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+                        isPaused = !isPaused;
+                    break;
+
                 default:
                     break;
             }
         }
 
 
+
         window.clear(sf::Color::Black);
 
-        /*test.detectCollisions();
-        test.resolveCollisions();
-        test.drawSpheres(window);
-        test.integratePositions(0.1f);*/
-        window.draw(shape);
+        if(!isPaused)
+        {
+            test.detectCollisions();
+            test.resolveCollisions();
+            test.integratePositions(0.1f);
+
+        }
+        //window.draw(emptySprite);
+            test.drawSpheres(window);
 
         window.display();
+
+        sf::sleep(sf::milliseconds(30));
     }
 }
